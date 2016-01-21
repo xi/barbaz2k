c36343e6d0f6ba3d2528318b6f5f490df4131c25
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-01-21 01:01:50 +0100

    change files structure

--- server.py
+++ server.py
@@ -22,18 +22,33 @@ def files_route(request):
             if filename.split('.')[-1] in ['mp3', 'ogg', 'flac', 'm4a']:
                 paths.append(os.path.join(dirpath, filename))
 
-    tree = {}
+    tree = {
+        'dirs': [],
+        'files': [],
+    }
 
-    for path in paths:
+    for path in sorted(paths):
         parts = path[len(MUSIC_DIR)+1:].split('/')
         head = tree
 
         for part in parts[:-1]:
-            if part not in head:
-                head[part] = {}
-            head = head[part]
+            for item in head['dirs']:
+                if item['title'] == part:
+                    head = item
+                    break
+            else:
+                item = {
+                    'title': part,
+                    'dirs': [],
+                    'files': [],
+                }
+                head['dirs'].append(item)
+                head = item
 
-        head[parts[-1]] = path
+        head['files'].append({
+            'title': parts[-1],
+            'path': path[len(MUSIC_DIR):],
+        })
 
     return jsonify(tree)
 
