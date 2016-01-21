f58b12ebc5ec00e3cdbf339d3908fdd827027e1e
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-01-21 01:04:41 +0100

    add filelist

--- static/index.js
+++ static/index.js
@@ -1,19 +1,32 @@
 (function(muu, xhr, Mustache, _) {
     'use strict';
 
-    var registry = new muu.Registry({
-        renderer: Mustache.render
-    });
-
     Promise.all([
         xhr.get('/static/foobar.html'),
+        xhr.get('/static/filelist.html'),
         xhr.getJSON('/files.json'),
     ]).then(function(args) {
         var template = args[0];
-        var files = args[1];
+        var files = args[2];
+
+        var partials = {
+            filelist: args[1],
+        };
+
+        var registry = new muu.Registry({
+            renderer: function(a, b) {
+                return Mustache.render(a, b, partials);
+            }
+        });
 
         registry.registerDirective('foobar', template, function(self) {
-            self.update({});
+            var update = function() {
+                self.update({
+                    files: files,
+                });
+            };
+
+            update();
         });
 
         registry.linkAll(document);
